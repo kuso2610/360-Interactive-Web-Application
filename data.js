27c95c362585dd6c988c8ae1e933200eff1f08e4
kuso2610 <58974706+kuso2610@users.noreply.github.com>
2022-08-13 15:41:05 +0100

    Update v2

--- data.js
+++ data.js
@@ -1,8 +1,55 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-8mechant-24",
-      "name": "8Mechant 24",
+      "id": "0-lounge-bar",
+      "name": "Lounge Bar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.0485185496805283,
+        "pitch": -0.011823119062917442,
+        "fov": 1.9023398572240051
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5847243366213952,
+          "pitch": -0.04519047434038548,
+          "rotation": 0,
+          "target": "1-toilet"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8657914418785584,
+          "pitch": -0.01189134568164718,
+          "title": "Damage Area",
+          "text": "Remove all items&nbsp;"
+        },
+        {
+          "yaw": 2.2579234302368665,
+          "pitch": -0.06955892815884823,
+          "title": "Bar Area",
+          "text": "Remove all items"
+        }
+      ]
+    },
+    {
+      "id": "1-toilet",
+      "name": "Toilet",
       "levels": [
         {
           "tileSize": 256,
@@ -25,21 +72,14 @@ var APP_DATA = {
         "fov": 1.5707963267948966
       },
       "linkHotspots": [],
-      "infoHotspots": [
-        {
-          "yaw": 1.504548710796163,
-          "pitch": -0.11520140434078385,
-          "title": "Test",
-          "text": "Testing"
-        }
-      ]
+      "infoHotspots": []
     }
   ],
   "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
+    "fullscreenButton": true,
     "viewControlButtons": false
   }
 };
